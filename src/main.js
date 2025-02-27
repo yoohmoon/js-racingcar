9fd33c8ccebf167676ed7a3a88127cd215aa341f
yoohmoon <121086010+yoohmoon@users.noreply.github.com>
2025-02-27 19:10:49 +0900

    💬 texts (경주) : 클래스 공부 주석 추가

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+/* 
+- 자동차는 이름을 상태로 가질 수 있다.
+- 자동차는 위치 값을 가지며, 초기 상태는 0이다.
+- 자동차는 전진할 수 있으며 한 번에 1만큼 전진한다.
+*/
+
 class Car {
   constructor(name) {
+    // constructor 생성자 역할(객체 초기화) : 인스턴스를 만들 때 실행된다.
     this.name = name;
     this.position = 0;
   }
 
   move() {
-    this.position += 1;
+    this.position += 1; // 현재 값에 오른쪽 값을 더한 후, 그 결과를 다시 변수에 할당
   }
 }
+
+const myCar = new Car("sonata"); //인스턴스 만듦. Car { name: 'sonata', position: 0 }
